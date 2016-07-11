992c837818cd1d0b9a77d5b51ffee9decdf8ccfe
Renan Barbieri <renan.hbarbieri@gmail.com>
2016-07-11 16:06:48 -0300

    Adicionando timer nas notificacoes

--- js/notification.js
+++ js/notification.js
@@ -1,9 +1,16 @@
+var intervalo = 30 * 1000;//30 segundos
+
+//essa funcao jah seta o timer. A gente guarda a variavel para poder limpar o timer, se quiser.
+var myTimer = window.setInterval(notifyMe, intervalo);
+
 // request permission on page load
 document.addEventListener('DOMContentLoaded', function () {
     console.log('ok');
-    if (Notification.permission !== "granted")
+    if (Notification.permission !== "granted"){
         Notification.requestPermission();
-    });
+    }
+    // timerNotification();
+});
 
 function notifyMe() {
     if (!Notification) {
@@ -15,13 +22,13 @@ function notifyMe() {
         Notification.requestPermission();
     }
     else {
-        var notification = new Notification('Hello world title', {
+        var notification = new Notification('Ei!! Não se esquece de regar seu alecrim!!! :)', {
         icon: 'http://statick4.bseller.com.br/product/74507/694485_5104_Dancing_Groot_GLAM_1024x1024.png',
         body: "Hello Gaia, that's my first notification test!",
         });
 
         // notification.onclick = function () {
-        // window.open("http://stackoverflow.com/a/13328397/1269037");      
+        // window.open("http://foo.com/bar");      
         // };
     }
 }  
